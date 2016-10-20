a3f23073b31a796f348aad0ed6cb4316e2950756
DSanchez <david38sanchez@gmail.com>
2016-10-20 18:04:19 -0400

    Update for command `version` and `start`

--- DockerStack.py
+++ DockerStack.py
@@ -2,15 +2,24 @@ import argparse
 
 
 class DockerStack(argparse.Action):
-    def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        if nargs is not None:
-            raise ValueError("nargs not allowed")
-
-        super(DockerStack, self).__init__(option_strings, dest, **kwargs)
+    args = []
+    VERSION = 1.0
 
     def __call__(self, parser, namespace, values, option_string=None):
         print '%r %r %r' % (namespace, values, option_string)
 
-        setattr(namespace, self.dest, values)
-    # def __init__(self):
-    #     print "========= Welcome to Kaliop Docker Stack project ==========\n"
+    def __init__(self, parser):
+        self.parser = parser
+        self.args = parser.parse_args()
+        print vars(self.args)
+        if not vars(self.args):
+            print self.parser.print_help()
+            parser.exit(1)
+
+        super(DockerStack, self).__init__(self, parser)
+
+    def start(self):
+        print "========= Welcome to Kaliop Docker Stack project ==========\n"
+
+    def version(self):
+        print "Docker Stack version", self.VERSION
--- stack.py
+++ stack.py
@@ -6,15 +6,21 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument('--start', '-s', action=DockerStack)
-    parser.add_argument('--version', action='version', version='version 1')
+    parser.add_argument('--start', '-s', action='store_true')
+    parser.add_argument('--version', action='store_true')
 
-    return parser.parse_args()
+    return parser
 
 
 def main():
-    parse_args()
+    parser = parse_args()
 
+    docker_stack = DockerStack(parser)
+
+    if docker_stack.args.version:
+        docker_stack.version()
+    elif docker_stack.args.start:
+        docker_stack.start()
 
 if __name__ == '__main__':
     main()
