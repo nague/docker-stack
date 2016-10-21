dc9ed5475ef3a5ef8902b618ae35cff7459160a8
DSanchez <david38sanchez@gmail.com>
2016-10-21 16:58:44 -0400

    Update commands

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
 Docker Stack by Kaliop Canada
 =============================
 
+Usage
+-----
+### Start
+Build and start a new project
+```bash
+./stash.py -s project
+```
+
+### Build
+Build a new or existing project
+```bash
+./stash.py -b project
+```
+
+### Run
+Run docker container(s), assuming files has been built
+```bash
+./stash.py -r project1 project2
+```
+
+### Stop
+Stop docker container(s) for the current project
+```bash
+./stash.py -s project1 project2
+```
+
+### Remove
+Remove one or more projects
+```bash
+./stash.py -rm project1 project2
+```
+
+### List projects
+List all created projects
+```bash
+./stash.py -ps
+```
+
+### Version
+Display version number
+```bash
+./stash.py -v
+```
+
+
 Dependencies
 ------------
 * Python 2.7.x
--- dockerstack/stack.py
+++ dockerstack/stack.py
@@ -6,15 +6,13 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description="")
-    # parser.add_argument('-b', '--build', action='store_true', help='build a project without running the container')
-    # parser.add_argument('-r', '--run', action='store_true',
-    #                     help='run docker container(s), assuming files has been built')
-    # parser.add_argument('-o', '--stop', action='store_true', help='stop docker container(s) for the current project')
-    # parser.add_argument('-t', '--reset', action='store_true', help='remove docker container(s) and all generated files')
 
     commands = parser.add_argument_group('Commands')
     commands.add_argument('-s', nargs='?', const=1, type=str, help='Build and start a new project')
-    commands.add_argument('-ps', help='List projects and status')
+    commands.add_argument('-b', nargs='?', const=1, type=str, help='Build a new or existing project')
+    commands.add_argument('-r', nargs='*', help="Run docker container(s), assuming files has been built")
+    commands.add_argument('-o', action='store_true', help='Stop docker container(s) for the current project')
+    commands.add_argument('-ps', action='store_true', help='List all created projects')
     commands.add_argument('-rm', nargs='?', const=1, help='Remove one or more projects')
     commands.add_argument('-v', action='store_true', help='Display version number')
 
@@ -30,6 +28,14 @@ def main():
     # Commands
     if docker_stack.args.s:
         docker_stack.start()
+    elif docker_stack.args.b:
+        pass
+    elif docker_stack.args.r:
+        pass
+    elif docker_stack.args.o:
+        pass
+    elif docker_stack.args.ps:
+        pass
     elif docker_stack.args.rm:
         docker_stack.remove()
     elif docker_stack.args.v:
