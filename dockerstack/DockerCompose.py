d29125183963ab826da7c4a875fea98714382c31
DSanchez <david38sanchez@gmail.com>
2016-10-26 14:07:36 -0400

    Fix class, execute return full output data when executing command

--- dockerstack/DockerCompose.py
+++ dockerstack/DockerCompose.py
@@ -1,27 +1,44 @@
-from subprocess import Popen, PIPE
+from __future__ import print_function
+import subprocess
 
 
 class DockerCompose(object):
+    DOCKER_COMPOSE_CMD = 'docker-compose'
+    SUDO_CMD = '/usr/bin/sudo'
 
     # Start containers
     def start(self, project):
-        p = Popen(['/usr/bin/sudo', 'docker-compose', '-p', project, 'up', '-d'], stdout=PIPE)
-        return p.stdout.read()
+        for path in self.execute([self.SUDO_CMD, self.DOCKER_COMPOSE_CMD, '-p', project, 'up', '-d']):
+            print(path, end="")
 
     # Build or rebuild services
-    def build(self):
-        pass
+    def build(self, project):
+        for path in self.execute([self.SUDO_CMD, self.DOCKER_COMPOSE_CMD, '-p', project, 'build']):
+            print(path, end="")
 
     # Stop services
     def stop(self, project):
-        p = Popen(['/usr/bin/sudo', 'docker-compose', '-p', project, 'stop'], stdout=PIPE)
-        return p.stdout.read()
-
-    #
-    def run(self):
-        pass
+        for path in self.execute([self.SUDO_CMD, self.DOCKER_COMPOSE_CMD, '-p', project, 'stop']):
+            print(path, end="")
 
     # Remove services
     def rm(self, project):
-        p = Popen(['/usr/bin/sudo', 'docker-compose', '-p', project, 'rm', '-f'], stdout=PIPE)
-        return p.stdout.read()
+        for path in self.execute([self.SUDO_CMD, self.DOCKER_COMPOSE_CMD, '-p', project, 'rm', '-f']):
+            print(path, end="")
+
+    # Generator: execute command and yield result
+    def execute(self, cmd):
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+        stdout_lines = iter(p.stdout.readline, "")
+        for stdout_line in stdout_lines:
+            yield stdout_line
+
+        p.stdout.close()
+        return_code = p.wait()
+        if return_code != 0:
+            raise subprocess.CalledProcessError(return_code, cmd)
+
+    # Return version information
+    def version(self):
+        return subprocess.Popen([self.SUDO_CMD, self.DOCKER_COMPOSE_CMD, 'version'],
+                                stdout=subprocess.PIPE).stdout.read()
