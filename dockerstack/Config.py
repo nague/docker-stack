9fd262950ca9705af28f80055b8ea7e6d1b73c4c
DSanchez <david38sanchez@gmail.com>
2016-10-29 17:00:14 -0400

    Fixes issue #1, check required and optional values, raise Exception for requires

--- dockerstack/Config.py
+++ dockerstack/Config.py
@@ -10,13 +10,19 @@ class Config(object):
     DEFAULT_TIMEZONE = 'America/Toronto'
     config_parser = ConfigParser.ConfigParser()
 
+    # ===========
     # Constructor
+    # ===========
     def __init__(self, path):
         self.config_path = path
+        self.config_parser.read(self.config_path)
         self.env = Environment(loader=PackageLoader('dockerstack', 'templates'))
 
+    # =================
     # Parse config file
+    # =================
     def parse_config(self):
+        # Return array with key
         array = {
             'docker': {},
             'docker-compose': {
@@ -25,41 +31,66 @@ class Config(object):
             },
             'php': {}
         }
-        self.config_parser.read(self.config_path)
+
+        # List required sections/options from 'docker-stack.ini'
+        required = {
+            'webserver': ['engine', 'vhost'],
+            'php': ['version']
+        }
+        # Checking default required sections/options exists
+        for section, options in required.items():
+            # 1. Check section exists
+            if not self.config_parser.has_section(section):
+                raise Exception('Error, section "{}" is required'.format(section))
+            # 2. Check option exists
+            for option in options:
+                if not self.config_parser.has_option(section, option):
+                    raise Exception('Error, option "{}" in section "{}" is required'.format(option, section))
+
+        # Get main sections
         general = self.config_section_map('general')
+        webserver = self.config_section_map('webserver')
+        php = self.config_section_map('php')
 
         # Database path
         if self.config_parser.has_option('general', 'db_path'):
             array['db'] = general['db_path']
 
         # Dockerfile variables
-        array['docker']['image'] = self.config_section_map('php')['version']
-        array['docker']['vhost'] = self.config_section_map('webserver')['vhost']
-        array['docker']['server_engine'] = self.config_section_map('webserver')['engine']
-        array['docker']['site'] = os.path.basename(array['docker']['vhost'])
-        array['docker']['libs'] = string.split(self.config_section_map('general')['libs'], ',')
-        array['docker']['extensions'] = string.split(self.config_section_map('php')['extensions'], ',')
-        array['docker']['type'] = self.config_section_map('general')['type']
+        array['docker']['server_engine'] = webserver['engine']
+        array['docker']['vhost'] = webserver['vhost']
+        array['docker']['site'] = os.path.basename(webserver['vhost'])
+        array['docker']['image'] = php['version']
+        if self.config_parser.has_option('general', 'libs'):
+            array['docker']['libs'] = string.split(general['libs'], ',')
+        if self.config_parser.has_option('php', 'extensions'):
+            array['docker']['extensions'] = string.split(php['extensions'], ',')
+        if self.config_parser.has_option('general', 'platform'):
+            array['docker']['platform'] = general['platform']
         if self.config_parser.has_option('php', 'enable'):
-            array['docker']['enable'] = string.split(self.config_section_map('php')['enable'], ',')
+            array['docker']['enable'] = string.split(php['enable'], ',')
         if self.config_parser.has_option('php', 'pecl'):
-            array['docker']['pecl'] = string.split(self.config_section_map('php')['pecl'], ',')
+            array['docker']['pecl'] = string.split(php['pecl'], ',')
 
         # docker-compose.yml variables
-        array['docker-compose']['ports'] = string.split(self.config_section_map('webserver')['ports'], ',')
-        for k, v in self.config_section_map('services').items():
-            array['docker-compose']['links'][v] = self.config_section_map(k)['link']
-            array['docker-compose']['services'][v] = self.config_section_map(k)
+        if self.config_parser.has_option('webserver', 'ports'):
+            array['docker-compose']['ports'] = string.split(webserver['ports'], ',')
+        if self.config_parser.has_section('services'):
+            for k, v in self.config_section_map('services').items():
+                array['docker-compose']['links'][v] = self.config_section_map(k)['link']
+                array['docker-compose']['services'][v] = self.config_section_map(k)
 
         # php.ini variables
         if self.config_parser.has_option('php', 'timezone'):
-            array['php']['timezone'] = self.config_section_map('php')['timezone']
+            array['php']['timezone'] = php['timezone']
         else:
             array['php']['timezone'] = self.DEFAULT_TIMEZONE
 
         return array
 
+    # =====================
     # Get config by section
+    # =====================
     def config_section_map(self, section):
         array = {}
         options = self.config_parser.options(section)
@@ -73,7 +104,9 @@ class Config(object):
                 array[option] = None
         return array
 
+    # ==================
     # Build php.ini file
+    # ==================
     def build_php_ini(self, source, destination):
         tmpl = self.env.get_template(source)
         if not os.path.exists(os.path.dirname(destination)):
