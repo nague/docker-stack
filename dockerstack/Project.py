4ecde294f5a861d6a067f59fb291f689728ae057
DSanchez <david38sanchez@gmail.com>
2016-11-02 16:12:05 -0400

    Add license on 1st launch

--- dockerstack/Project.py
+++ dockerstack/Project.py
@@ -39,16 +39,47 @@ class Project(object):
     # Constructor
     # ===========
     def __init__(self):
-        # Welcome message when stating app
-        print "========= Welcome to {} by {}  ==========".format(dockerstack.__name__, dockerstack.__maintainer__)
-
         # Create main config directory, it will contains logs, ...
         if not os.path.exists(self.CONFIG_DIRECTORY):
             os.makedirs(self.CONFIG_DIRECTORY)
 
         # Create projects directory, it will contains all docker-stack projects
         if not os.path.exists(self.PROJECTS_DIRECTORY):
-            os.makedirs(self.PROJECTS_DIRECTORY)
+            print """================== Welcome to {} by {}  ==================
+
+Copyright (c) 2016, David Sanchez / Kaliop Canada
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+* Neither the name of Docker Stack nor the names of its contributors may be
+  used to endorse or promote products derived from this software
+  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY KALIOP CANADA ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL KALIOP CANADA BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+==============================================================================
+            """.format(dockerstack.__name__, dockerstack.__maintainer__)
+            validation = yesno("Do you agree to terms? [Y/n]: ", default=True)
+            if validation is True:
+                os.makedirs(self.PROJECTS_DIRECTORY)
+            else:
+                exit(1)
 
     # =============================
     # Get project name from options
